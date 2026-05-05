6a1457d928bf90a53b3a73e1332c7e3b8de2cc7b
offluisz <moreiraluis2904@gmail.com>
2026-05-05 10:44:30 -0300

    Revert "Feature/cart service"

--- routes/ProdutoRouter.js
+++ routes/ProdutoRouter.js
@@ -40,7 +40,6 @@ router.get('/listar', ctrl.listar);
 router.get('/alterar/:id', ctrl.AlterarView);
 router.post('/alterar', upload.single('img'), ctrl.alterar);
 router.post('/excluir', ctrl.excluir);
-router.get('/obter/:produtoId', ctrl.obterProduto);
 
 
 module.exports = router;
